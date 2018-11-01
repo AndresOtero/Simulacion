24b4dda96cd4553819c715fd7d1d066565c227d8
andres <oteroandres95@gmail.com>
2018-11-01 16:45:44 -0300

    Ploteo el sistema dinamico en un grafico

--- TP2/Sistemas Dinamicos/simularSistemaDinamico.py
+++ TP2/Sistemas Dinamicos/simularSistemaDinamico.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
-listNumbers=[-1,0,1]
+listNumbers=[1,0,-1]
 possibleStates= [ (x,y,z) for x in listNumbers for y in listNumbers for z in listNumbers]
 arrayTrajectories={}
 for possState in possibleStates:
@@ -14,14 +14,23 @@ for possState in possibleStates:
 	dinamicSystem=DinamicSystem(state)
 	arrayTrajectories[possState]=dinamicSystem.listNextState_N_Steps(100)
 
-for possState in arrayTrajectories:
-	fig = plt.figure()
-	ax = fig.gca(projection='3d')
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+for possState in arrayTrajectories: 
+	ax.scatter3D(possState[0],possState[1],possState[2])
 	x= [ x[0] for x in arrayTrajectories[possState]]
 	y= [ x[1] for x in arrayTrajectories[possState]]
 	z= [ x[2] for x in arrayTrajectories[possState]]
-	ax.plot(x, y, z, label='Diagrama de fases')
-	ax.legend()
-	plt.grid(True)		
-	plt.savefig(str(possState)+".png")
-	plt.show()
+	ax.plot(x, y, z, label=str(possState))
+
+x= [ 0 for x in range(100)]
+y= [ 0 for x in range(100)]
+z= [ x/5.0 for x in range(-50,50)]
+ax.plot(x, y, z, label="eje z")
+ax.legend()
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+plt.grid(True)		
+plt.savefig(str("todo")+".png")
+plt.show()
