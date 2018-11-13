495af67f077b59b1cf2fa30aad3e605c38c780d3
Facundo <facundo.pichinini@gmail.com>
2018-11-13 15:46:07 -0300

    cleaning

--- TP2/Markov/ej2.py
+++ TP2/Markov/ej2.py
@@ -5,8 +5,6 @@ q = 1/40
 
 simulador = Simulador.SimulacionMarkov(p,q,30,0)
 simulador.simular(100000)
-#print("Cantidad iteraciones: ", simulador.iteraciones())
-#simulador.printResultado()
 print("Porcentae de tiempo sin procesar solicitudes: ", simulador.getIdleTime())
 simulador.mostrarHistogramaEstados("Estado","Cantidad","Histograma de Estados","./ej2-hist.png")
 simulador.graficarEstados("Iteracion","Estado","Evolución de Estados","./ej2-grafico.png")
